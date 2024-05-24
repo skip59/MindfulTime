3a2a78b64bb90c488d52a867dc6a9f89f3283ea6
skip59@yandex.ru <skip59@yandex.ru>
2024-05-24 17:59:12 +0500

    ++

--- MindfulTime.Notification.Domain/Repository/Entities/Message.cs
+++ MindfulTime.Notification.Domain/Repository/Entities/Message.cs
@@ -1,6 +1,15 @@
-﻿namespace MindfulTime.Notification.Domain.Repository.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MindfulTime.Notification.Domain.Repository.Entities
 {
     public class Message
     {
+        [Key]
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string MethodSend { get; set; }
+        public DateTime Created { get; set; }
+
     }
 }
