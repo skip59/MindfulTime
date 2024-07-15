64eb9b1df2d40c5112e4f447f4039f36b6622896
Yulianna <lulittill@gmail.com>
2024-07-15 16:08:21 +0500

    logging auth

---
+++ MindfulTime.Auth/ExampleSchemaFilter.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace MindfulTime.Auth.App;
+
+internal class ExampleSchemaFilter : ISchemaFilter
+{
+    private readonly JsonSerializerSettings _settings = new()
+    {
+        Formatting = Formatting.Indented,
+        NullValueHandling = NullValueHandling.Ignore,
+        Converters = new List<JsonConverter> { new Newtonsoft.Json.Converters.StringEnumConverter() }
+    };
+
+    private static readonly ProblemDetails _problemDetailsObject = new()
+    {
+        Type = "Microsoft.AspNetCore.Http.BadHttpRequestException",
+        Title = "One or more validation errors occurred",
+        Status = 400
+    };
+
+    private static readonly UserDto _templateDtoObject = new()
+    {
+        Id = Guid.NewGuid(),
+        Name = "Test",
+        Email = "test@mail.com",
+        Password = "Password",
+        IsSendMessage = true,
+        Role = "admin",
+        TelegramId = "0"
+    };
+
+    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    {
+        object example = context.Type.Name switch
+        {
+            nameof(ProblemDetails) => _problemDetailsObject,
+            nameof(UserDto) => _templateDtoObject,
+            _ => null
+        };
+
+        if (example is not null)
+            schema.Example = new OpenApiString(JsonConvert.SerializeObject(example, _settings));
+    }
+}
--- MindfulTime.Auth/Program.cs
+++ MindfulTime.Auth/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.OpenApi.Models;
+using MindfulTime.Auth.App;
+using System.Text.RegularExpressions;
+using Serilog;
+
 namespace MindfulTime.Auth;
 
 public class Program
@@ -5,18 +11,44 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Host.UseSerilog((context, services, configuration) => configuration
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services));
+
         string connection = builder.Configuration.GetConnectionString("UserDatabase");
         builder.Services.AppAuthContext(connection);
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+            options.CustomOperationIds(e => $"{Regex.Replace(e.RelativePath, "{|}", "")}{e.HttpMethod}");
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "MindfulTime.Auth",
+                Version = "v1",
+                Description = $"Service Authorization"
+            });
+            var xmlFiles = Directory.GetFiles(AppContext.BaseDirectory, "*.xml", SearchOption.TopDirectoryOnly).ToList();
+            xmlFiles.ForEach(xmlFile => options.IncludeXmlComments(xmlFile));
+
+            options.SchemaFilter<ExampleSchemaFilter>();
+
+        });
+        builder.Services.AddSwaggerGenNewtonsoftSupport();
+
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        if (!app.Environment.IsProduction())
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger(o => o.SerializeAsV2 = true);
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", $"MindfulTime.Auth v1");
+            });
         }
 
         app.UseHttpsRedirection();
